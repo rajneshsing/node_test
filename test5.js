742890d6a0d5bb5b9d759149582251dbe2f4ae93
rajnesh <rajneshmmm@gmail.com>
2021-09-29 16:15:21 +0530

    first commit

--- test5.js
+++ test5.js
@@ -63,11 +63,41 @@ async function print(readable) {
 const ress=print(fs.createReadStream('test.txt')).then((value)=> { console.log(value) }).catch(console.error);*/
 
 
-var fs = require("fs");  
-// Create a readable stream  
-var readerStream = fs.createReadStream('test.txt');  
+  
+
+/*var fs = require("fs");  
+var data = 'A Solution of all Technology';  
 // Create a writable stream  
-var writerStream = fs.createWriteStream('output.txt');  
-// Pipe the read and write operations  
-// read input.txt and write data to output.txt  
-readerStream.pipe(writerStream);  
+var writerStream = fs.createWriteStream('testt.txt');  
+// Write the data to stream with encoding to be utf8  
+writerStream.write(data,'UTF8');  
+// Mark the end of file  
+writerStream.end();  
+// Handle stream events --> finish, and error  
+writerStream.on('finish', function() {  
+    console.log("Write completed.");  
+});  
+writerStream.on('error', function(err){  
+   console.log(err.stack);  
+});  
+console.log("Program Ended");*/  
+
+var fs = require("fs");  
+var data = '';  
+// Create a readable stream  
+var readerStream = fs.createReadStream('test11.txt');  
+// Set the encoding to be utf8.   
+readerStream.setEncoding('UTF8');  
+// Handle stream events --> data, end, and error  
+readerStream.on('data', function(chunk) {  
+   data += chunk;  
+});  
+readerStream.on('end',function(){  
+   console.log(data);  
+});  
+readerStream.on('error', function(err){  
+   console.log(err.stack);  
+}); 
+
+ 
+console.log("Program Ended");  
